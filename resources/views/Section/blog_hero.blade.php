1a437896ff6a3bb9171daf89b5b2f5bd0d874d5d
samudradhk <samudradhika@gmail.com>
2026-05-05 21:46:03 +0700

    first creat blog page

---
+++ resources/views/Section/blog_hero.blade.php
@@ -0,0 +1,205 @@
+<div class="relative w-full max-w-5xl mx-auto h-[550px] rounded-xl overflow-hidden shadow-2xl mt-10">
+    
+    <div id="carousel-slides" class="w-full h-full relative">
+        
+        <div id="carousel-slides" class="w-full h-full relative">
+        
+        <div class="carousel-item cursor-pointer absolute inset-0 transition-opacity duration-700 ease-in-out opacity-100 z-10">
+            <img src="{{ asset('assets/hero1.jpg') }}" class="w-full h-full object-cover" alt="Borobudur">
+            
+            <div class="absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent"></div>
+            
+            <div class="absolute inset-0 p-12 text-white">
+                
+                <div class="absolute top-10 left-10">
+                    <span class="bg-[#1A365D] px-5 py-2.5 rounded-full text-xs font-semibold flex items-center w-max gap-2.5">
+                        ⭐ Featured Story
+                    </span>
+                </div>
+
+                <div class="absolute bottom-12 left-10 right-10">
+                    <h2 class="text-4xl md:text-5xl font-bold leading-tight mb-3">Sunrise Over the <br> Majestic Borobudur Temple</h2>
+                    
+                    <div class="flex items-center gap-2.5 text-sm text-gray-300 mb-6">
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 21C16.9706 17.3824 20 13.3824 20 9C20 4.58172 16.4183 1 12 1C7.58172 1 4 4.58172 4 9C4 13.3824 7.02944 17.3824 12 21Z" stroke="currentColor" stroke-width="2"/><circle cx="12" cy="9" r="3" stroke="currentColor" stroke-width="2"/></svg>
+                            Magelang, Central Java
+                        </span>
+                        
+                        <span class="text-gray-500">|</span>
+                        
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 4H5C3.89543 4 3 4.89543 3 6V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4Z" stroke="currentColor" stroke-width="2"/><path d="M16 2V6M8 2V6M3 10H21" stroke="currentColor" stroke-width="2"/><path d="M11 13H13V15H11V13Z" stroke="currentColor" stroke-width="2"/></svg>
+                            August 20, 2023 by Samudra Bryandhika
+                        </span>
+                    </div>
+
+                    <button class="bg-[#1A365D] hover:bg-blue-800 transition px-6 py-2.5 rounded-full text-sm font-semibold">
+                        Read Full Story
+                    </button>
+                </div>
+            </div>
+        </div>
+        
+        <div class="carousel-item cursor-pointer absolute inset-0 transition-opacity duration-700 ease-in-out opacity-100 z-10">
+            <img src="{{ asset('assets/hero2.jpg') }}" class="w-full h-full object-cover" alt="Borobudur">
+            
+            <div class="absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent"></div>
+            
+            <div class="absolute inset-0 p-12 text-white">
+                
+                <div class="absolute top-10 left-10">
+                    <span class="bg-[#1A365D] px-5 py-2.5 rounded-full text-xs font-semibold flex items-center w-max gap-2.5">
+                        ⭐ Featured Story
+                    </span>
+                </div>
+
+                <div class="absolute bottom-12 left-10 right-10">
+                    <h2 class="text-4xl md:text-5xl font-bold leading-tight mb-3">A Taste of Indonesia: <br>
+                        Exploring Traditional Cuisine</h2>
+                    
+                    <div class="flex items-center gap-2.5 text-sm text-gray-300 mb-6">
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 21C16.9706 17.3824 20 13.3824 20 9C20 4.58172 16.4183 1 12 1C7.58172 1 4 4.58172 4 9C4 13.3824 7.02944 17.3824 12 21Z" stroke="currentColor" stroke-width="2"/><circle cx="12" cy="9" r="3" stroke="currentColor" stroke-width="2"/></svg>
+                            Badung, Bali
+                        </span>
+                        
+                        <span class="text-gray-500">|</span>
+                        
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 4H5C3.89543 4 3 4.89543 3 6V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4Z" stroke="currentColor" stroke-width="2"/><path d="M16 2V6M8 2V6M3 10H21" stroke="currentColor" stroke-width="2"/><path d="M11 13H13V15H11V13Z" stroke="currentColor" stroke-width="2"/></svg>
+                            April, 2023 by Clarissa Jesselyn
+                        </span>
+                    </div>
+
+                    <button class="bg-[#1A365D] hover:bg-blue-800 transition px-6 py-2.5 rounded-full text-sm font-semibold">
+                        Read Full Story
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item cursor-pointer absolute inset-0 transition-opacity duration-700 ease-in-out opacity-100 z-10">
+            <img src="{{ asset('assets/hero3.jpg') }}" class="w-full h-full object-cover" alt="Borobudur">
+            
+            <div class="absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent"></div>
+            
+            <div class="absolute inset-0 p-12 text-white">
+                
+                <div class="absolute top-10 left-10">
+                    <span class="bg-[#1A365D] px-5 py-2.5 rounded-full text-xs font-semibold flex items-center w-max gap-2.5">
+                        ⭐ Featured Story
+                    </span>
+                </div>
+
+                <div class="absolute bottom-12 left-10 right-10">
+                    <h2 class="text-4xl md:text-5xl font-bold leading-tight mb-3">Tropical Escape to <br>
+                        Pink Beach, Labuan Bajo</h2>
+                    
+                    <div class="flex items-center gap-2.5 text-sm text-gray-300 mb-6">
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 21C16.9706 17.3824 20 13.3824 20 9C20 4.58172 16.4183 1 12 1C7.58172 1 4 4.58172 4 9C4 13.3824 7.02944 17.3824 12 21Z" stroke="currentColor" stroke-width="2"/><circle cx="12" cy="9" r="3" stroke="currentColor" stroke-width="2"/></svg>
+                            Pink Beach, NTT
+                        </span>
+                        
+                        <span class="text-gray-500">|</span>
+                        
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 4H5C3.89543 4 3 4.89543 3 6V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4Z" stroke="currentColor" stroke-width="2"/><path d="M16 2V6M8 2V6M3 10H21" stroke="currentColor" stroke-width="2"/><path d="M11 13H13V15H11V13Z" stroke="currentColor" stroke-width="2"/></svg>
+                            January 04, 2025 by Rio Dwi
+                        </span>
+                    </div>
+
+                    <button class="bg-[#1A365D] hover:bg-blue-800 transition px-6 py-2.5 rounded-full text-sm font-semibold">
+                        Read Full Story
+                    </button>
+                </div>
+            </div>
+       </div>
+       
+        <div class="carousel-item cursor-pointer absolute inset-0 transition-opacity duration-700 ease-in-out opacity-100 z-10">
+            <img src="{{ asset('assets/hero4.jpg') }}" class="w-full h-full object-cover" alt="Borobudur">
+            
+            <div class="absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent"></div>
+            
+            <div class="absolute inset-0 p-12 text-white">
+                
+                <div class="absolute top-10 left-10">
+                    <span class="bg-[#1A365D] px-5 py-2.5 rounded-full text-xs font-semibold flex items-center w-max gap-2.5">
+                        ⭐ Featured Story
+                    </span>
+                </div>
+
+                <div class="absolute bottom-12 left-10 right-10">
+                    <h2 class="text-4xl md:text-5xl font-bold leading-tight mb-3">The Grace and Beauty <br>
+                        of Traditional Indonesian Dance</h2>
+                    
+                    <div class="flex items-center gap-2.5 text-sm text-gray-300 mb-6">
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 21C16.9706 17.3824 20 13.3824 20 9C20 4.58172 16.4183 1 12 1C7.58172 1 4 4.58172 4 9C4 13.3824 7.02944 17.3824 12 21Z" stroke="currentColor" stroke-width="2"/><circle cx="12" cy="9" r="3" stroke="currentColor" stroke-width="2"/></svg>
+                            Padang, West Sumatra
+                        </span>
+                        
+                        <span class="text-gray-500">|</span>
+                        
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 4H5C3.89543 4 3 4.89543 3 6V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4Z" stroke="currentColor" stroke-width="2"/><path d="M16 2V6M8 2V6M3 10H21" stroke="currentColor" stroke-width="2"/><path d="M11 13H13V15H11V13Z" stroke="currentColor" stroke-width="2"/></svg>
+                            August 24, 2024 by Samuel Christian
+                        </span>
+                    </div>
+
+                    <button class="bg-[#1A365D] hover:bg-blue-800 transition px-6 py-2.5 rounded-full text-sm font-semibold">
+                        Read Full Story
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item cursor-pointer absolute inset-0 transition-opacity duration-700 ease-in-out opacity-100 z-10">
+            <img src="{{ asset('assets/hero5.jpg') }}" class="w-full h-full object-cover" alt="Borobudur">
+            
+            <div class="absolute inset-0 bg-gradient-to-t from-black/90 via-black/40 to-transparent"></div>
+            
+            <div class="absolute inset-0 p-12 text-white">
+                
+                <div class="absolute top-10 left-10">
+                    <span class="bg-[#1A365D] px-5 py-2.5 rounded-full text-xs font-semibold flex items-center w-max gap-2.5">
+                        ⭐ Featured Story
+                    </span>
+                </div>
+
+                <div class="absolute bottom-12 left-10 right-10">
+                    <h2 class="text-4xl md:text-5xl font-bold leading-tight mb-3">Hidden Paradise: <br>
+                    Discovering Indonesia’s Waterfalls</h2>
+                    
+                    <div class="flex items-center gap-2.5 text-sm text-gray-300 mb-6">
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 21C16.9706 17.3824 20 13.3824 20 9C20 4.58172 16.4183 1 12 1C7.58172 1 4 4.58172 4 9C4 13.3824 7.02944 17.3824 12 21Z" stroke="currentColor" stroke-width="2"/><circle cx="12" cy="9" r="3" stroke="currentColor" stroke-width="2"/></svg>
+                            Sentul, West Java
+                        </span>
+                        
+                        <span class="text-gray-500">|</span>
+                        
+                        <span class="flex items-center gap-1.5">
+                            <svg class="w-4 h-4 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 4H5C3.89543 4 3 4.89543 3 6V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4Z" stroke="currentColor" stroke-width="2"/><path d="M16 2V6M8 2V6M3 10H21" stroke="currentColor" stroke-width="2"/><path d="M11 13H13V15H11V13Z" stroke="currentColor" stroke-width="2"/></svg>
+                            July 17, 2025 by Ong, Jason
+                        </span>
+                    </div>
+
+                    <button class="bg-[#1A365D] hover:bg-blue-800 transition px-6 py-2.5 rounded-full text-sm font-semibold">
+                        Read Full Story
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex space-x-3 z-20">
+        <button class="carousel-dot w-3 h-3 rounded-full bg-orange-400 transition" onclick="goToSlide(0)"></button>
+        <button class="carousel-dot w-3 h-3 rounded-full bg-white transition" onclick="goToSlide(1)"></button>
+        <button class="carousel-dot w-3 h-3 rounded-full bg-white transition" onclick="goToSlide(2)"></button>
+        <button class="carousel-dot w-3 h-3 rounded-full bg-white transition" onclick="goToSlide(3)"></button>
+        <button class="carousel-dot w-3 h-3 rounded-full bg-white transition" onclick="goToSlide(4)"></button>
+    </div>
+</div>
